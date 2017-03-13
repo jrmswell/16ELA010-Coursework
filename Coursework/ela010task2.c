a6ec8630ab43dc60da5d12ac338e2e8a18bd105b
James Marsh <cheesytoast@live.co.uk>
2017-03-13 13:49:30 +0000

    Setting up ready to start using arrays

--- Coursework/ela010task2.c
+++ Coursework/ela010task2.c
@@ -3,13 +3,14 @@
 
 main()
 {
+	int array[12][4];
 	FILE *f = fopen("file.ppm", "w");
 	if (f == NULL)
 	{
 		printf("Oops, we cannot open the specified file\n");
 		exit(1);
 	}
-	const char *text = "P3\n4 4\n15\n0 0 0 0 0 0 0 0 0 15 0 15\n0 0 0 0 15 7 0 0 0 0 0 0\n0 0 0 0 0 0 0 15 7 0 0 0\n15 0 15 0 0 0 0 0 0 0 0 0";
+	const char *text = "P3\n4 4\n15\n0 0 0 0 0 0 0 0 0 15 0 15\n0 0 0 0 15 7 0 0 0 0 0 0\n0 0 0 0 0 0 0 15 7 0 0 0\n15 0 15 0 0 0 0 0 0 0 0 0"; //
 	fprintf(f, "%s\n", text);
 	fclose(f);
 }
