a2224beeaf6f359f6dc6c6603f89d04e13dcdbf7
James Marsh <cheesytoast@live.co.uk>
2017-03-15 20:38:56 +0000

    Found some documentation reguarding math.h and sine waves

--- Coursework/ela010task2.c
+++ Coursework/ela010task2.c
@@ -3,14 +3,21 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <Windows.h>
+#include <wchar.h>
 #define width 256
 #define height 256
-#define colour 256
+#define colour 255
 #define PI 3.14159265
 
 int main()
 {
-	int i, j, k, array[width][height][3];
+	int i, j, k, array[width][height][3], y, x;
+	for (x = 0; x < width; x++)
+	{
+		y = (height / 2) + (height / 2) * sin(2 * PI * (x / (double)width));
+		array[y][x][0] = 255, array[y][x][1] = 128, array[y][x][2] = 0;
+	}
 	FILE *f = fopen("file.ppm", "w");
 	if (f == NULL)
 	{
@@ -19,16 +26,10 @@ int main()
 	}
 	fprintf(f, "P3\n%d %d\n%d\n", width, height, colour);
 	for (i = 0; i < width; i++)
-	{
 		for (j = 0; j < height; j++)
 		{
-			array[i][j][0] = i;
-			array[i][j][1] = j;
-			array[i][j][2] = j;
 			fprintf(f, "%d %d %d ", array[i][j][0], array[i][j][1], array[i][j][2]);
 		}
-		fprintf(f, "\n");
-	}
 	fclose(f);
 	return 0;
 }
