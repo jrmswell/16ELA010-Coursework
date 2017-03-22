6cf52151f1d8ffdcbeb34e361e8e7183c4afea92
James Marsh <cheesytoast@live.co.uk>
2017-03-22 11:49:04 +0000

    Some minor useability updates
    Allows for the user to set the amount of cycles that will occur during the course of the program. Will try and get the for loop to return to the line above next time

--- Coursework/ela010task2.c
+++ Coursework/ela010task2.c
@@ -12,10 +12,18 @@
 
 int main()
 {
-	int i, j, k, array[width][height][3], y, x;
+	int i, j, cycles, array[width][height][3], y, x;
+	printf("How many cycles would you like?\t");
+	scanf("%d", &cycles);
+	if (cycles > 7)
+	{
+		printf("%d is too high. Please select a value of 7 or less\n", cycles);
+		exit(1);
+
+	}
 	for (x = 0; x < width; x++)
 	{
-		y = (height / 2) + (height / 2) * sin(2 * PI * (x / (double)width));
+		y = (height / 2) + (height / 2) * sin(2 * PI * (x*cycles / (double)width));
 		array[y][x][0] = 255, array[y][x][1] = 128, array[y][x][2] = 0;
 	}
 	FILE *f = fopen("file.ppm", "w");
