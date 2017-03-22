eaf5eaf6b643b2524f778a71bb506cd74eb5b9ab
James Marsh <cheesytoast@live.co.uk>
2017-03-22 11:51:39 +0000

    Did the last thing

--- Coursework/ela010task2.c
+++ Coursework/ela010task2.c
@@ -15,10 +15,11 @@ int main()
 	int i, j, cycles, array[width][height][3], y, x;
 	printf("How many cycles would you like?\t");
 	scanf("%d", &cycles);
-	if (cycles > 7)
+	while (cycles > 7)
 	{
 		printf("%d is too high. Please select a value of 7 or less\n", cycles);
-		exit(1);
+		printf("How many cycles would you like?\t");
+		scanf("%d", &cycles);
 
 	}
 	for (x = 0; x < width; x++)
